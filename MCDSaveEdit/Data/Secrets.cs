2d03573272d9c15edf5dce9747ac01e983e1213d
LuckyPants12 <75674528+LuckyPants12@users.noreply.github.com>
2022-04-24 11:42:33 +0530

    Add files via upload

---
+++ MCDSaveEdit/Data/Secrets.cs
@@ -0,0 +1,14 @@
+namespace MCDSaveEdit
+{
+    public static class Secrets
+    {
+        // Fill in the value for this one
+        public static string[] PAKS_AES_KEY_STRINGS = new string[] {
+            "0x16D28F03446CB9E15665F11EF5A0D6820A6483C083CA6DF1AE9370D197ACB588",
+        };
+
+        // You can leave these empty, they just need to exist
+        public const string GAME_ANALYTICS_GAME_KEY = "";
+        public const string GAME_ANALYTICS_SECRET_KEY = "";
+    }
+}
